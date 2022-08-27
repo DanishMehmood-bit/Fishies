5cbc8995694cb2778d698eff9ee73234597c66b8
Danish Mehmood <Danish.Mehmood@bentley.com>
2022-08-27 22:10:23 +0500

    Fixed: Bubble flash

--- scripts/bubbleHandler.js
+++ scripts/bubbleHandler.js
@@ -2,26 +2,27 @@ import { Bubble } from "./Bubble.js";
 import { Utils } from "./Utils.js";
 
 export const bubbleHandler = (gameFrame, bubblesGenerated, canvas, player, score) => {
-  let bubblesToRemove = [];
+  const updateDrawBubble = (bubble) => {
+    bubble.update();
+    bubble.draw();
+  }
 
   if (gameFrame % 50 === 0)
     bubblesGenerated.push(new Bubble(canvas));
 
   bubblesGenerated.forEach((bubble, index) => {
-    bubble.update();
-    bubble.draw();
+    updateDrawBubble(bubble);
 
     const isPlayerColliding = Utils.isColliding(bubble, player);
 
-    if (bubble.isAtTop() || isPlayerColliding)
-      bubblesToRemove.push(index);
+    if (bubble.isAtTop() || isPlayerColliding) {
+      bubblesGenerated.splice(index, 1);
+      // Once we remove the bubble the bubbles after that move back one index so the bubble after the removed one
+      // will not update or draw so we get the flash
+      updateDrawBubble(bubblesGenerated[index]);
+    }
 
     if (isPlayerColliding)
       score.increment();
-  });
-
-  // To remove the bubbles flashing, we need to remove them separately from the updating loop 
-  bubblesToRemove.forEach(toRemoveIndex => {
-    bubblesGenerated.splice(toRemoveIndex, 1);
   });
 }
